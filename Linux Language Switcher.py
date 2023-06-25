d60ef8cf6617d273c32c3c5da8b76bf7692c8653
Mohammad Ansarifar <132356369+M-Ansarifar@users.noreply.github.com>
2023-06-25 12:28:32 +0330

    Update Linux Language Switcher.py
    Main Source

--- Linux Language Switcher.py
+++ Linux Language Switcher.py
@@ -1,7 +1,8 @@
 # Linux persian languge changer
 import os, time
 
-print ("Developed by Mo-Ans\n")
+print ("Initializing...\n")
+# Sleep
 time.sleep(1.8)
 os.system("setxkbmap -layout us,ir")
 os.system("setxkbmap -option 'grp:alt_shift_toggle'")
